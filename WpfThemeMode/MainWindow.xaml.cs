d7f1507b331402083f6bc76b9b4a1a35371e1cd1
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2024-12-17 02:15:51 +0900

    テーマモードのラジオボタンとDataGridの追加
    MainWindow.xamlに新しいテーマモードのRadioButtonグループを追加しました。このグループには「System」、「Dark」、「Light」の3つのRadioButtonが含まれており、ThemeModeプロパティにバインドされています。BoolToThemeModeConverterを使用して変換されます。
    
    既存の「Radio 1」、「Radio 2」、「Radio 3」のRadioButtonグループを削除しました。
    
    新しいDataGridグループを追加しました。このDataGridはItemsプロパティにバインドされており、セルのスタイルがカスタマイズされています。
    
    MainWindow.xaml.csにCommunityToolkit.Mvvm.Messagingの使用を追加し、WeakReferenceMessengerを使用してThemeModeプロパティの変更を監視し、更新するロジックを追加しました。
    
    MainWindow.xaml.csに#pragmaディレクティブを追加し、WPF0001警告を無効化しました。

--- WpfThemeMode/MainWindow.xaml.cs
+++ WpfThemeMode/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Windows;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace WpfThemeMode;
 
+#pragma warning disable WPF0001 // 種類は、評価の目的でのみ提供されています。将来の更新で変更または削除されることがあります。続行するには、この診断を非表示にします。
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
@@ -11,5 +14,11 @@ public partial class MainWindow : Window
 	public MainWindow()
 	{
 		InitializeComponent();
+
+		WeakReferenceMessenger.Default.Register<MainWindow, PropertyChangedMessage<ThemeMode>>( this, static ( window, message ) =>
+		{
+			window.ThemeMode = message.NewValue;
+		} );
 	}
 }
+#pragma warning restore WPF0001 // 種類は、評価の目的でのみ提供されています。将来の更新で変更または削除されることがあります。続行するには、この診断を非表示にします。
