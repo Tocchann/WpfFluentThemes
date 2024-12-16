7d50d8f484d6101b4690f94741d74e84e0fc2d1f
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2024-12-17 02:15:14 +0900

    BoolToThemeModeConverterの追加とリソース設定
    `App.xaml` ファイルに `BoolToThemeModeConverter` リソースを追加しました。
    `BoolToThemeModeConverter.cs` ファイルを新規作成し、`BoolToThemeModeConverter` クラスを定義しました。このクラスは `IValueConverter` インターフェースを実装し、ブール値をテーマモードに変換するロジックを提供します。

---
+++ WpfThemeMode/BoolToThemeModeConverter.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfThemeMode;
+#pragma warning disable WPF0001 // 種類は、評価の目的でのみ提供されています。将来の更新で変更または削除されることがあります。続行するには、この診断を非表示にします。
+
+public class BoolToThemeModeConverter : IValueConverter
+{
+	private ThemeModeConverter ThemeModeConverter { get; init; } = new();
+	public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+	{
+		var themeMode = parameter as string;
+		if( themeMode == null )
+		{
+			return System.Windows.DependencyProperty.UnsetValue;
+		}
+		var obj = ThemeModeConverter.ConvertFromString( themeMode );
+		if( obj == null )
+		{
+			return System.Windows.DependencyProperty.UnsetValue;
+		}
+		var themeModeValue = ThemeModeConverter.ConvertFrom( value );
+		return Object.Equals( themeModeValue, obj );
+	}
+
+	public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+	{
+		if( value is bool isChecked && isChecked && parameter is string paramStr )
+		{
+			var obj = ThemeModeConverter.ConvertFromString( paramStr );
+			if( obj != null )
+			{
+				return (ThemeMode)obj;
+			}
+		}
+		return System.Windows.DependencyProperty.UnsetValue;
+	}
+}
+#pragma warning restore WPF0001 // 種類は、評価の目的でのみ提供されています。将来の更新で変更または削除されることがあります。続行するには、この診断を非表示にします。
